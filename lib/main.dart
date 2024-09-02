169cc4652603e3cff96ff6294bda73a052fbd6a7
ZiannF <Andrianmarhas@gmail.com>
2024-09-02 12:12:28 +0700

    catatan perubahan

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:latihan3/page1.dart';
+import 'package:latihan3/page2.dart';
 
 void main() {
   runApp(const MyApp());
@@ -31,10 +33,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      routes: {
-        '/': (context) => page1(),
-        '/page2': (context) => page2()
-      },
+      routes: {'/': (context) => Page1(), '/page2': (context) => Page2()},
       initialRoute: '/',
     );
   }
--- lib/page2.dart
+++ lib/page2.dart
@@ -1,13 +1,42 @@
 import 'package:flutter/material.dart';
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
+class Page2 extends StatelessWidget {
+  const Page2({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Ini Halaman 2'),
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Container(
+                height: 100,
+                width: 100,
+                color: Colors.red,
+              ),
+              Container(
+                height: 100,
+                width: 100,
+                color: Colors.green,
+              )
+            ],
+          ),
+          Row(
+            children: [
+              Container(
+                height: 100,
+                width: 100,
+                color: Colors.blue,
+              ),
+              Container(
+                height: 100,
+                width: 100,
+                color: Colors.amber,
+              )
+            ],
+          )
+        ],
       ),
     );
   }
